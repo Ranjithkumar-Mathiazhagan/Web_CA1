7518725372a48eb89b9506fc58cac3828ddd9229
Ranjithkumar-Mathiazhagan <swayakrishna7@gmail.com>
2024-07-09 15:36:29 +0530

    update

--- mern-ecommerce-main/server/server.js
+++ mern-ecommerce-main/server/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000
 // middlewares
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-app.use(express.static("public"));
+// app.use(express.static("public"));
 app.use(cors());
 // connect to the mongodb database
 connectDB();
